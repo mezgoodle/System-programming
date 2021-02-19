122f2dfe4f9ef45b2dbd470fe355dcbcba2c3152
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-02-19 13:06:23 +0200

    Some updates

--- lab1.asm
+++ lab1.asm
@@ -2,138 +2,68 @@
 .model flat, stdcall
 option casemap: none
  
-include /masm32/include/windows.inc
-include /masm32/include/user32.inc
-include /masm32/include/kernel32.inc
-includelib /masm32/lib/user32.lib
-includelib /masm32/lib/kernel32.lib
+include /masm32/include/masm32rt.inc
+
+.data?
+	
+	buff db 128 dup(?)
+	buff_d db 32 dup(?)
+	buff_d_n db 32 dup(?)
+	buff_e db 32 dup(?)
+	buff_e_n db 32 dup(?)
+	buff_f db 32 dup(?)
+	buff_f_n db 32 dup(?)
+
 
 .data
     dlg_header DB "Lab 1", 0
 	
+	form db "Symbol = %d" ,10, "A = %d", 10, "-A = %d", 10, "B = %d", 10, "-B = %d", 10, "C = %d", 10, "-C = %d", 10, "D = %s", 10, "-D = %s", 10,"E = %s", 10, "-E = %s", 10, "F = %s", 10, "-F = %s", 0 
+	
 	; Whole number:
-    symbol_str DB "%s (Symbol)", 0
-    symbol_buf DB 128 DUP (?)
-    symbol DB "0911200", 0
+    symbol DD 0911200
 	
 	;A:
-    a_byte_string DB "A = 09 = %x (Byte)", 0
-    a_byte_buffer DB 32 DUP (?)
-    a_byte DB +09
-	
-    a_negative_byte_string DB "-A = -09 = %x (Byte)", 0
-    a_negative_byte_buffer DB 32 DUP (?)
-    a_negative_byte DB -09
+    a DD +09
+    a_negative DD -09
 	
 	;B:
-    b_word_string DB "B = 0911 = %x (Byte)", 0
-    b_word_buffer DB 32 DUP (?)
-    b_word DW +0911
-	
-	b_negative_word_string DB "-B = -0911 = %x (Byte)", 0
-    b_negative_word_buffer DB 32 DUP (?)
-    b_negative_word DW -0911
+    b DD +0911
+    b_negative DD -0911
 	
 	;C:
-	c_shortint_string DB "C = 09112001 = %x (Shortint)", 0
-    c_shortint_buffer DB 64 DUP (?)
-    c_shortint DD +09112001
-	
-    c_negative_shortint_string DB "-C = -09112001 = %x (Shortint)", 0
-    c_negative_shortint_buffer DB 64 DUP (?)
-    c_negative_shortint DD -09112001
-	
-	c_longint_string DB "C = 09112001 = %x (Longint)", 0
-    c_longint_buffer DB 64 DUP (?)
-    c_longint DQ +09112001
-	
-    c_negative_longint_string DB "-C = -09112001 = %x (Longint)", 0
-    c_negative_longint_buffer DB 64 DUP (?)
-	c_negative_longint DQ -09112001
+    c_ DD +09112001
+    c_negative DD -09112001
 	
 	;D:
-	d_single_string DB "D = 0,001 = %x (Single)", 0
-    d_single_buffer DB 64 DUP (?)
-    d_single DD +0.001
-	
-    d_negative_single_string DB "-D = -0,001 = %x (Single)", 0
-    d_negative_single_buffer DB 64 DUP (?)
-    d_negative_single DD -0.001
+    d DQ +0.001
+    d_negative DQ -0.001
 	
 	;E:
-	e_double_string DB "E = 0,098 = %x%x (Double)", 0
-    e_double_buffer DB 32 DUP (?)
-    e_double DQ +0.098
-	
-    e_negative_double_string DB "-E = -0,098 = %x%x (Double)", 0
-    e_negative_double_buffer DB 32 DUP (?)
-    e_negative_double DQ -0.098
+    e DQ +0.098
+    e_negative DQ -0.098
 	
 	;F:
-	f_long_double_string DB "F = 978.522 = %x%x (Long Double)", 0
-    f_long_double_buffer DB 64 DUP (?)
-    f_long_double DT +978.522
+    f DQ +978.522
+    f_negative DQ -978.522
 	
-    f_negative_long_double_string DB "-F = -978.522 = %x%x (Long Double)", 0
-    f_negative_long_double_buffer DB 64 DUP (?)
-    f_negative_long_double DT -978.522
-	
-
-    msg_str DB "%s", 13,
-        "%s", 13,
-		"%s", 13,
-		"%s", 13,
-		"%s", 13,
-		"%s", 13,
-		"%s", 13,
-		"%s", 13,
-		"%s", 13,
-		"%s", 13,
-		"%s", 13,
-		"%s", 13,
-		"%s", 13,
-		"%s", 13,
-		"%s", 13,
-        0
-
-    msg_buf DB 128 DUP (?)
 	
 .code
     start:
-	invoke wsprintf, addr symbol_buf, addr symbol_str, addr symbol
-	
-	invoke wsprintf, addr a_byte_buffer, addr a_byte_string, a_byte
-	invoke wsprintf, addr a_negative_byte_buffer, addr a_negative_byte_string, a_negative_byte
-	invoke wsprintf, addr b_word_buffer, addr b_word_string, b_word
-	invoke wsprintf, addr b_negative_word_buffer, addr b_negative_word_string, b_negative_word
-	invoke wsprintf, addr c_shortint_buffer, addr c_shortint_string, c_shortint
-	invoke wsprintf, addr c_negative_shortint_buffer, addr c_negative_shortint_string, c_negative_shortint
-	invoke wsprintf, addr c_longint_buffer, addr c_longint_string, c_longint
-	invoke wsprintf, addr c_negative_longint_buffer, addr c_negative_longint_string, c_negative_longint
-	invoke wsprintf, addr d_single_buffer, addr d_single_string, d_single
-	invoke wsprintf, addr d_negative_single_buffer, addr d_negative_single_string, d_negative_single
-	invoke wsprintf, addr e_double_buffer, addr e_double_string, e_double
-	invoke wsprintf, addr e_negative_double_buffer, addr e_negative_double_string, e_negative_double
-	invoke wsprintf, addr f_long_double_buffer, addr f_long_double_string, f_long_double
-	invoke wsprintf, addr f_negative_long_double_buffer, addr f_negative_long_double_string, f_negative_long_double
+	invoke FloatToStr2, d, addr buff_d
+	invoke FloatToStr2, d_negative, addr buff_d_n
+	invoke FloatToStr2, e, addr buff_e
+	invoke FloatToStr2, e_negative, addr buff_e_n
+	invoke FloatToStr2, f, addr buff_f
+	invoke FloatToStr2, f_negative, addr buff_f_n
 
-	invoke wsprintf, addr msg_buf, addr msg_str,
-        addr symbol_buf,
-        addr a_byte_buffer,
-	    addr a_negative_byte_buffer,
-		addr b_word_buffer,
-		addr b_negative_word_buffer,
-		addr c_shortint_buffer,
-		addr c_negative_shortint_buffer,
-		addr c_longint_buffer,
-		addr c_negative_longint_buffer,
-		addr d_single_buffer,
-		addr d_negative_single_buffer,
-		addr e_double_buffer,
-		addr e_negative_double_buffer,
-		addr f_long_double_buffer,
-		addr f_negative_long_double_buffer
-		
-	invoke MessageBox, 0, addr msg_buf, addr dlg_header, MB_OK
+	invoke wsprintf, addr buff, addr form, symbol, a, a_negative, b, b_negative, c_, c_negative, 
+		addr buff_d,
+		addr buff_d_n,
+		addr buff_e,
+		addr buff_e_n,
+		addr buff_f,
+		addr buff_f_n
+	invoke MessageBox, 0, addr buff, addr dlg_header, MB_OK
     invoke ExitProcess, 0
     end start
