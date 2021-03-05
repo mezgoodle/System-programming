241b82d2b306884aad3e35c18ab750f7eb70e33d
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-03-05 14:16:06 +0200

    Update start.bat

--- Lab2/start.bat
+++ Lab2/start.bat
@@ -1,7 +1,7 @@
 set folder=%~dp0
-set masm=C:\masm32\bin
-set dosbox=C:\DosBox\DOSBox-0.74-3\DOSBox.exe
-set filename=%1
+set masm=%1		rem C:\masm32\bin
+set dosbox=%2   rem C:\DosBox\DOSBox-0.74-3\DOSBox.exe
+set filename=%3 rem Lab2
 
 %masm%\ml /Bl %masm%\link16.exe %folder%%filename%.asm
 %dosbox% -c "mount c %folder% " -c c: -c "keyb none 866 " -c "%filename% EXE "
