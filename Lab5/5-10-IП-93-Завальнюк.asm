320b8d6dff5b05899c8b8b036684a3f6f46f81f2
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-04-14 17:26:33 +0300

    Update 5-10-IП-93-Завальнюк.asm

--- Lab5/5-10-IП-93-Завальнюк.asm
+++ Lab5/5-10-IП-93-Завальнюк.asm
@@ -1,226 +1,130 @@
-.386
-.model flat, stdcall
-option casemap :none   
+INCLUDE \masm32\include\masm32rt.inc
 
-include /masm32/include/windows.inc
-include /masm32/include/user32.inc
-include /masm32/include/kernel32.inc
-includelib /masm32/lib/user32.lib
-includelib /masm32/lib/kernel32.lib
+; (c / b - 24 + a) / (2 * a * c - 1)
+calc MACRO a, b, d
+    MOV AL, 2  ; preparing multiplication
+    IMUL a      ; 2 * a       -> AL
+    IMUL d   ; 2 * a * c      -> AL
+    DEC AL  ; 2 * a * c - 1 -> AL
+    MOV res, AL   ; 2 * a * c - 1 -> res
+	
+    MOV AL, 1   ; preparing multiplication
+    IMUL d      ; 1 * с         -> AL
+    IDIV b    ; 1 * с / b     -> AL
+    SUB AL, 24      ; 1 * c / b - 24 -> AL
+	ADD AL, a   ; 1 * c / b - 24 + a
+	
+    IDIV res ; (c / b - 24 + a) / (2 * a * c - 1) -> AL, AH = 0
+ENDM
+
+finalCalc MACRO n, buffer
+    LOCAL odd
+    LOCAL fin
+
+    MOV BL, n
+    SAR BL, 1
+    JB odd
+
+    INVOKE wsprintf, addr buffer, addr msg_even_format, BL
+    JMP fin
+
+    odd:
+    MOV AL, 5
+    IMUL n
+    INVOKE wsprintf, addr buffer, addr msg_odd_format, AL
+    
+    fin:
+ENDM
+
+printNum MACRO n, buffer
+    LOCAL pos
+    LOCAL fin
+    MOV     CL, n
+    TEST    CL, CL
+    JNS     pos
+
+    NEG CL
+    INVOKE wsprintf, addr buffer, addr msg_neg_format, CL
+    JMP fin
+
+    pos:
+    INVOKE wsprintf, addr buffer, addr msg_pos_format, CL
+
+    fin:
+ENDM
+
+getExpression MACRO i, buff
+    printNum a[i], buff_a
+    printNum b[i], buff_b
+    printNum d[i], buff_d
+
+    calc a[i], b[i], d[i]
+    MOV res, AL
+    printNum AL, buff_res
+    
+    finalCalc res, buff_res_final
+
+    INVOKE wsprintf, buff, addr msg_final,
+        addr buff_b,
+        addr buff_d,
+        addr buff_a,
+        addr buff_a,
+        addr buff_d,
+        addr buff_res,
+        addr buff_res_final
+ENDM
 
 .data
-	; Password
-	password db "nlgc`"
-	length_of_password dw 5
-	KEY DB 9h
-	; Attributes variables for windows
-	static_attr db "static", 0
-	edit_attr db "edit", 0
-    button_attr db "button", 0
-	; Text variables
-	student_name db "ПІБ: Завальнюк М.Є.", 0
-	student_number db "Номер залікової книги: 9312", 0
-	student_date db "Дата народження: 09.11.2001", 0
-	; Text for windows
-	greeting_text db "Введіть пароль:", 0
-	error_text db "Помилка! Перевірте введений вами пароль.", 0
-	on_button_text db "Ввести", 0
-	hInstance 		dd ?
-	lpszCmdLine		dd ?
-	edit_field 		HWND ?
-	input_text db 64 DUP (?)
+    msg_title       DB "Лабораторна робота 5", 0
+    msg_last_final  DB "Результати обчислень:", 10,
+        "1. %s", 10,
+        "2. %s", 10,
+        "3. %s", 10,
+        "4. %s", 10,
+        "5. %s", 0
+    msg_final              DB "(%s / %s - 24 + %s) / (2 * %s * %s - 1) = %s -> %s", 0
+    msg_neg_format         DB "(-%d)", 0
+    msg_pos_format         DB "%d", 0
+    msg_odd_format         DB "* 5 = %d", 0
+    msg_even_format        DB "/ 2 = %d", 0
 
-Main_WINDOW proto :dword, :dword, :dword, :dword
-Our_WINDOW proto :dword, :dword, :dword, :dword	
+    buff_last_final        DB 512 DUP (0)
+    buff_final             DB 064 DUP (0)
+    buff_final_2           DB 064 DUP (0)
+    buff_final_3           DB 064 DUP (0)
+    buff_final_4           DB 064 DUP (0)
+    buff_final_5           DB 064 DUP (0)
+    buff_a                 DB 008 DUP (0)
+    buff_b                 DB 008 DUP (0)
+    buff_d                 DB 008 DUP (0)
+    buff_res               DB 008 DUP (0)
+    buff_res_final         DB 016 DUP (0)
+    current_buff_addr      DD 0
 
-szText macro name, text:vararg
-	; Macros for changing text
-	;; Useless thing
-	local lbl
-	jmp	lbl
-	name db text, 0
-	lbl:
-endm
-
-SHOW_TEXT macro text, y_ordinate
-	; Macros for showing
-	;;Show data in window interface
-	invoke CreateWindowEx,NULL,
-        addr static_attr,
-        text,
-        WS_VISIBLE or WS_CHILD or SS_CENTER,
-        75, y_ordinate, 150, 100, hWnd, 4483, hInstance, NULL
-endm
-
-COMPARE macro input
-	; Macros for comparing
-	;;Comparing the input text and password
-	local COMPARING
-	local QUIT
-	MOV di, -1
-	; Main part
-    COMPARING:
-	INC di
-    CMP di, length_of_password
-	JE QUIT
-	MOV BL, input[di]
-	MOV BH, password[di]
-    CMP BL, BH
-    JNE SHOW_ERROR
-	LOOP COMPARING
-	QUIT:
-endm
-
-DECRYPT macro input
-	; Macros for decrypting
-	;; Decrypting the input text
-	local DECRYPTING
-	local QUIT
-	MOV di, 0
-	; Main part
-    DECRYPTING:
-	MOV dh, KEY
-    XOR input[di], dh
-    INC di
-    CMP di, length_of_password
-    JE QUIT
-	LOOP DECRYPTING
-	QUIT:
-endm
-
+    res                    DB 0
+    a                      DB -3, -3, -3, -3, -3
+    b                      DB 2, 2, 2, 2, 2
+    d                      DB 4, 2, 4, 4, 4
 .code
-start:
-	invoke 	GetModuleHandle, NULL
-	MOV	hInstance, eax
+    start:
+        MOV EDI, 0
+        MOV current_buff_addr, offset buff_final
 
-	invoke	GetCommandLine
-	MOV	lpszCmdLine, eax
+        hereWeGoAgain:
+        getExpression EDI, current_buff_addr
 
-	invoke 	Main_WINDOW, hInstance, NULL, lpszCmdLine, SW_SHOWDEFAULT
-	invoke	ExitProcess, eax
+        ADD current_buff_addr, 64
+        INC EDI
+        CMP EDI, 5
+        JB hereWeGoAgain
 
-Main_WINDOW proc hInst:dword, hPrevInst:dword, szCmdLine:dword, nShowCmd:dword
-	; Our main window
-	local 	wc 	:WNDCLASSEX
-	local 	msg 	:MSG
-	local 	hWnd 	:HWND
+        INVOKE wsprintf, addr buff_last_final, addr msg_last_final,
+            addr buff_final,
+            addr buff_final_2,
+            addr buff_final_3,
+            addr buff_final_4,
+            addr buff_final_5
 
-	; Set title texts with macroses
-	szText	szClassName, "Main_Window"
-	szText	szWindowTitle, "Лабораторна номер 4"
-	
-	MOV	wc.cbSize, sizeof WNDCLASSEX
-	MOV	wc.style, CS_HREDRAW or CS_VREDRAW or CS_BYTEALIGNWINDOW
-	MOV wc.lpfnWndProc, Our_WINDOW
-	MOV wc.cbClsExtra, NULL
-	MOV	wc.cbWndExtra, NULL
-
-	PUSH hInst
-	POP wc.hInstance
-
-	MOV	wc.hbrBackground, COLOR_BTNFACE + 1
-	MOV	wc.lpszMenuName, NULL
-	MOV wc.lpszClassName, offset szClassName
-
-	invoke LoadIcon, hInst, IDI_APPLICATION
-	MOV	wc.hIcon, eax
-	MOV	wc.hIconSm, eax
-
-	invoke LoadCursor, hInst, IDC_ARROW
-	MOV	wc.hCursor, eax
-
-	invoke RegisterClassEx, ADDR wc
-
-	invoke CreateWindowEx, WS_EX_APPWINDOW, OFFSET szClassName, OFFSET szWindowTitle,
-				WS_OVERLAPPEDWINDOW, 
-				300, 300, 360, 263, 
-				NULL, NULL, hInst, NULL
-
-	MOV	hWnd, eax
-
-	invoke	ShowWindow, hWnd, nShowCmd
-	invoke	UpdateWindow, hWnd
-
-MessageCreating:
-	; Create the message window
-	;; Useless thing
-	invoke GetMessage, ADDR msg, NULL, 0, 0
-	CMP eax, 0
-	JE MessageDestructing
-	invoke TranslateMessage, ADDR msg
-	invoke DispatchMessage, ADDR msg
-	JMP MessageCreating
-
-MessageDestructing:
-	; Destruct the message window
-	;; Usefull thing
-	MOV	eax, msg.wParam
-	RET
-
-Main_WINDOW endp
-
-
-Our_WINDOW proc hWnd: dword, uMsg: dword, wParam: dword, lParam: dword
-	; Create our window
-	; Create the main window
-	.if uMsg==WM_CREATE
-	; Show greeting text
-		invoke CreateWindowEx,NULL,
-                OFFSET static_attr,
-                OFFSET greeting_text,
-                WS_VISIBLE or WS_CHILD or SS_CENTER,
-                0, 2, 140, 20, hWnd, 5146, hInstance, NULL
-	; Show input field for text
-		invoke CreateWindowEx,NULL,
-                OFFSET edit_attr,
-                NULL,
-                WS_VISIBLE or WS_CHILD or ES_LEFT or ES_AUTOHSCROLL or ES_AUTOVSCROLL or WS_BORDER,
-                140, 0, 100, 20, hWnd, 9273, hInstance, NULL 
-        MOV edit_field, eax
-	; Show button with text
-        invoke CreateWindowEx,NULL,
-                OFFSET button_attr,
-                OFFSET on_button_text,
-                WS_VISIBLE or WS_CHILD,
-				251, 0, 80, 20, hWnd, 44832, hInstance, NULL
-	; Close the window
-	.elseif uMsg == WM_CLOSE
-		invoke 	PostQuitMessage, 0
-		XOR	eax, eax
-		RET
-	; Main part
-	.elseif uMsg == WM_COMMAND
-    	CMP wParam, 44832
-    	JNE QUIT
-    	invoke SendMessage, edit_field, WM_GETTEXT, 40, OFFSET input_text
-		MOV edi, 0
-
-		; COMPARING
-		COMPARING:
-    	CMP ax, length_of_password
-		JNE SHOW_ERROR
-		; Call macroses
-		DECRYPT input_text
-		COMPARE input_text
-		cmp bl, 0
-		jne SHOW_DATA
-    	SHOW_ERROR:
-		; Show error while input text
-		SHOW_TEXT OFFSET error_text, 50
-    	JMP QUIT
-		
-    	SHOW_DATA:
-		; Show my data
-		SHOW_TEXT OFFSET student_name, 50
-		SHOW_TEXT OFFSET student_number, 70
-		SHOW_TEXT OFFSET student_date, 110
-	.else
-		invoke DefWindowProc, hWnd, uMsg, wParam, lParam 
-        RET
-	.endif
-		QUIT:
-    	XOR eax, eax
-    	RET
-Our_WINDOW endp
-
-end start
+        INVOKE MessageBox, 0, addr buff_last_final, addr msg_title, MB_OK
+        INVOKE ExitProcess, 0
+    END start
