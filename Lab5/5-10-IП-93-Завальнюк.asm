2e0686e4b01328f22b8c409c8e2af9e6191bfa60
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-04-15 21:14:08 +0300

    Update 5-10-IП-93-Завальнюк.asm

--- Lab5/5-10-IП-93-Завальнюк.asm
+++ Lab5/5-10-IП-93-Завальнюк.asm
@@ -1,89 +1,5 @@
 INCLUDE \masm32\include\masm32rt.inc
 
-; (c / b - 24 + a) / (2 * a * c - 1)
-calc MACRO a, b, d
-	xor ax,ax          ; очистили регистр ax
-    MOV AL, 2    ; в al - 2
-    IMUL a      ; 2 * a       -> AL
-	cbw
-    IMUL d   ; 2 * a * c      -> AL
-	cbw
-    DEC AL  ; 2 * a * c - 1 -> AL
-	cbw
-    MOV res, AL   ; 2 * a * c - 1 -> res
-	cbw
-    MOV AL, 1   ; preparing multiplication
-	cbw
-    IMUL d      ; 1 * с         -> AL
-	cbw
-    IDIV b    ; 1 * с / b     -> AL
-	cbw
-    SUB AL, 24      ; 1 * c / b - 24 -> AL
-	cbw
-	ADD AL, a   ; 1 * c / b - 24 + a
-	cbw
-    IDIV res ; (c / b - 24 + a) / (2 * a * c - 1) -> AL, AH = 0
-cbw
-	;cbw
-ENDM
-
-finalCalc MACRO n, buffer
-    LOCAL odd
-    LOCAL fin
-
-    MOV BL, n
-	cbw
-    SAR BL, 1
-	cbw
-    JB odd
-
-    INVOKE wsprintf, addr buffer, addr msg_even_format, BL
-    JMP fin
-
-    odd:
-    MOV AL, 5
-	cbw
-    IMUL n
-	cbw
-    INVOKE wsprintf, addr buffer, addr msg_odd_format, AL
-    
-    fin:
-ENDM
-
-printNum MACRO n, buffer
-    LOCAL pos
-    LOCAL fin
-    MOV     CL, n
-    TEST    CL, CL
-    JNS     pos
-
-    NEG CL
-    INVOKE wsprintf, addr buffer, addr msg_neg_format, CL
-    JMP fin
-
-    pos:
-    INVOKE wsprintf, addr buffer, addr msg_pos_format, CL
-
-    fin:
-ENDM
-
-getExpression MACRO i, buff
-    printNum coeffs_a[i], a_element
-    printNum coeffs_b[i], b_element
-    printNum coeffs_c[i], c_element
-
-    calc coeffs_a[i], coeffs_b[i], coeffs_c[i]
-    MOV res, AL
-    
-    finalCalc res, buff_res_final
-
-    INVOKE wsprintf, buff, addr msg_final,
-        addr a_element,
-        addr b_element,
-        addr c_element,
-        addr buff_res_final
-ENDM
-
 .data
     msg_title       DB "Лабораторна робота 5", 0
     msg_last_final  DB "1. %s", 10,
@@ -111,9 +27,95 @@ ENDM
     current_buff_addr      DD 0
 
     res                    DB 0
-    coeffs_a                      DB -3, -3, -2, -1, 3
-    coeffs_b                      DB 2, 2, -2, -1, -2
-    coeffs_c                      DB 4, 2, 2, 3, 4
+    coeffs_a               DB -3, -3, -2, -1, 3
+    coeffs_b               DB 2, 2, -2, -1, -2
+    coeffs_c               DB 4, 2, 2, 3, 4
+
+; (c / b - 24 + a) / (2 * a * c - 1)
+calculate_the_row MACRO a, b, c_
+	xor ax,ax
+    MOV AL, 1
+	INC AL
+    IMUL a
+	cbw
+    IMUL c_
+	cbw
+    DEC AL
+	cbw
+    MOV res, AL
+	cbw
+    MOV AL, 1
+	cbw
+    IMUL c_
+	cbw
+    IDIV b
+	cbw
+	ADD AL, a
+	cbw
+    SUB AL, 24
+	cbw
+    IDIV res
+	cbw
+ENDM
+
+finalCalc MACRO buffer, number
+    LOCAL odd
+    LOCAL fin
+
+    MOV BL, number
+	cbw
+    SAR BL, 1
+	cbw
+    JB odd
+
+    INVOKE wsprintf, addr buffer, addr msg_even_format, BL
+    JMP fin
+
+    odd:
+    MOV AL, 5
+	cbw
+    IMUL number
+	cbw
+    INVOKE wsprintf, addr buffer, addr msg_odd_format, AL
+    
+    fin:
+ENDM
+
+printNum MACRO buffer, number
+    LOCAL pos
+    LOCAL fin
+    MOV     CL, number
+    TEST    CL, CL
+    JNS     pos
+
+    NEG CL
+    INVOKE wsprintf, addr buffer, addr msg_neg_format, CL
+    JMP fin
+
+    pos:
+    INVOKE wsprintf, addr buffer, addr msg_pos_format, CL
+
+    fin:
+ENDM
+
+getExpression MACRO i, buff
+    printNum a_element, coeffs_a[i]
+    printNum b_element, coeffs_b[i]
+    printNum c_element, coeffs_c[i]
+
+    calculate_the_row coeffs_a[i], coeffs_b[i], coeffs_c[i]
+    MOV res, AL
+    
+    finalCalc buff_res_final, res
+
+    INVOKE wsprintf, buff, addr msg_final,
+        addr a_element,
+        addr b_element,
+        addr c_element,
+        addr buff_res_final
+ENDM
+
+
 .code
     start:
         MOV EDI, 0
